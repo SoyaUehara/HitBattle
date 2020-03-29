16a1d5f29a37b0a623b5534a105debd8ff515f67
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2020-03-29 15:22:47 +0900

    2nd

--- src/main/java/jp/ac/uryukyu/ie/e195752/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195752/Main.java
@@ -2,12 +2,19 @@ package jp.ac.uryukyu.ie.e195752;
 import java.util.ArrayList;
 public class Main {
     static public void main(String[] args){
-        ArrayList<ArrayList> Fire = new ArrayList<>();
-        ArrayList<ArrayList> water = new ArrayList<>();
-        ArrayList<ArrayList> wood = new ArrayList<>();
-        ArrayList<ArrayList> light = new ArrayList<>();
-        ArrayList<ArrayList> darkness = new ArrayList<>();
-        ArrayList<Integer> i = new ArrayList<>();
-        System.out.println(Fire);
+        int count = 0;
+        String f = "Wood";
+        Read fire = new Read(f);
+        //System.out.println(fire.getList());
+        for (int i=0; i<fire.getList().size(); i++){
+            if (count==4){
+                System.out.println(fire.getList().get(i));
+                count = 0;
+            }else {
+                System.out.print(fire.getList().get(i));
+                System.out.print(" ");
+                count++;
+            }
+        }
     }
 }
