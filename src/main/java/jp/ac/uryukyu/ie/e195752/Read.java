280c46dc6ed44c579882bb0c1d712935b08fc5b9
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2020-03-29 15:22:07 +0900

    2nd

---
+++ src/main/java/jp/ac/uryukyu/ie/e195752/Read.java
@@ -0,0 +1,25 @@
+package jp.ac.uryukyu.ie.e195752;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Read {
+    private ArrayList<String> List = new ArrayList<>();
+    public Read(String element){
+        try {
+            File file = new File(element+".txt");
+            String pass = file.getAbsolutePath();
+            File data = new File(pass);
+            Scanner scan = new Scanner(data);
+            while (scan.hasNext()) {
+                String str = scan.next();
+                this.List.add(str);
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("ファイルがありません");
+        }
+    }
+    public ArrayList getList(){
+        return this.List;
+    }
+}
